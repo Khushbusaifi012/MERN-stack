08ea8de2632df924db51f68977c25b53214aab22
khushbusaifi012 <khushbusaifi27@gmail.com>
2025-12-07 19:43:07 +0530

    Make counting counter

--- websites/React/IntroReact/react-project-one/src/App.jsx
+++ websites/React/IntroReact/react-project-one/src/App.jsx
@@ -2,15 +2,17 @@
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserCard from './components/UserCard'      
+import Counter from './components/Counter'
 
 function App() {
 
   return (
     <>
     <div className="container">
-      <UserCard name="saheen saifi" desc="desc1"  /> 
-      <UserCard  name="khushbu saifi" desc="desc2"/>
-      <UserCard name="Ayesha saifi" desc="desc3"/>
+      <UserCard name="saheen saifi" desc="I am Saheen Saifi"  /> 
+      <UserCard  name="khushbu saifi" desc="I am khushbu Saifi"/>
+      <UserCard name="Ayesha saifi" desc="I am Ayesha Saifi"/>
+      <Counter/>
     </div>
     </>
   )
